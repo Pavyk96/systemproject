84af593f725451c4306522aea1476d5185e19133
Mezev Daniil <pepethefrog27@mail.com>
2026-05-19 11:39:35 +0500

    Первичные настройки

--- src/main/java/naumen/daniilmezev/systemproject/SystemprojectApplication.java
+++ src/main/java/naumen/daniilmezev/systemproject/SystemprojectApplication.java
@@ -2,8 +2,10 @@ package naumen.daniilmezev.systemproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SystemprojectApplication {
 
 	public static void main(String[] args) {
